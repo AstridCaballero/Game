6547b7b97b51c8acd35fb7b3de673617e142d741
Anna Canby Monk <annacanbymonk@Annas-MBP.config>
2020-11-25 18:09:03 +0000

    2020.11.25.18.08

--- Ada-FuzzballSlam-main/class.js
+++ Ada-FuzzballSlam-main/class.js
@@ -1,15 +1,48 @@
 "use strict";
 
+class c_launcher {
+    constructor(x, y, body) { // see docs on http://brm.io/matter-js/docs/classes/Constraint.html#properties
+        let options = {
+            pointA: {
+                x: x,
+                y: y
+            },
+            bodyB: body, 
+            stiffness: 0.02,
+            length: 80
+        }
+        // create the constraint 
+        this.launch = Matter.Constraint.create(options);
+        Matter.World.add(world, this.launch); // add to the matter world
+
+    }
+    release() {
+        //release the constrained body by setting it to null
+        this.launch.bodyB = null;
+    }
+
+    show() {
+        //check to see if there is an active body
+        if(this.launch.bodyB) {
+            let posA = this.launch.pointA; // create a shortcut alias
+            let posB = this.launch.bodyB.position;
+            stroke("#00ff00"); // set a colour
+            line(posA.x, posA.y, posB.x, posB.y); //draw a line between the two points
+        }
+    }
+}
+
 class c_ground {
     constructor(x, y, width, height) {
         let options = {
             isStatic: true,
-            restitutions: 0.99,
+            restitution: 0.99,
             friction: 0.20,
             density: 0.99,
         }
+        //create the body 
         this.body = Matter.Bodies.rectangle(x, y, width, height, options);
-        Matter.World.add(world, this.body);
+        Matter.World.add(world, this.body); // add to the matter world
 
         this.x = x;
         this.y = y;
@@ -17,15 +50,15 @@ class c_ground {
         this.height = height;
     }
     body() {
-        return this.body;
+        return this.body; // return the created body
     }
 
     show() {
-        const pos = this.body.position;
-        noStroke();
-        fill('#ffffff');
+        let pos = this.body.position; // create a shortcut alias
         rectMode(CENTER); // switch centre to be centre rather than left, top
-        rect(pos.x, pos.y, this.width, this.height)
+        fill("#ffffff"); // set the fill colour
+        rect(pos.x, pos.y, this.width, this.height) // draw the rectangle 
+        //noStroke(); // THIS ISN'T IN MIKE's Tues iteration of the code
     }
 }
 
@@ -33,13 +66,14 @@ class c_ground {
 class c_crate {
     constructor(x, y, width, height) {
         let options = {            
-            restitutions: 0.99,
+            restitution: 0.99,
             friction: 0.030,
             density: 0.99,
             frictionAir: 0.032,
         }
+        // create the body
         this.body = Matter.Bodies.rectangle(x, y, width, height, options);
-        Matter.World.add(world, this.body);
+        Matter.World.add(world, this.body); // add to the matter world
 
         this.x = x;
         this.y = y;
@@ -47,19 +81,19 @@ class c_crate {
         this.height = height;
     }
     body() {
-        return this.body;
+        return this.body; //return the created body 
     }
 
     show() {
-        const pos = this.body.position;
-        const angle = this.body.angle;
+        let pos = this.body.position; // create a shortcut alias
+        let angle = this.body.angle;
 
         push(); // p5 translation
+            stroke("#000000");
+            fill("#ffffff");
+            rectMode(CENTER); // switch centre to be centre rather than left, top
             translate(pos.x, pos.y);
             rotate(angle);
-            noStroke();
-            fill('#ffffff');
-            rectMode(CENTER); // switch centre to be centre rather than left, top
             rect(0, 0, this.width, this.height)
         pop();
     }
@@ -69,7 +103,7 @@ class c_crate {
 class c_fuzzball {
     constructor(x, y, diameter) {
         let options = {            
-            restitutions: 0.90,
+            restitution: 0.90,
             friction: 0.005,
             density: 0.95,
             frictionAir: 0.005,
@@ -81,19 +115,20 @@ class c_fuzzball {
         this.y = y;
         this.diameter = diameter;
     }
+
     body() {
         return this.body;
     }
 
     show() {
-        const pos = this.body.position;
-        const angle = this.body.angle;
+        let pos = this.body.position;
+        let angle = this.body.angle;
 
         push(); // p5 translation
             translate(pos.x, pos.y);
             rotate(angle);
-            noStroke();
-            fill('#ffffff');
+            // noStroke(); This isn't in Mike's Ties iteration
+            fill("#ffffff");
             ellipseMode(CENTER); // switch centre to be centre rather than left, top
             circle(0, 0, this.diameter);
         pop();
--- Ada-FuzzballSlam-main/script.js
+++ Ada-FuzzballSlam-main/script.js
@@ -1,41 +1,50 @@
 "use strict";
 
-var vp_width = 920, vp_height = 690;
-var world, engine, body;
+var vp_width = 920, vp_height = 690;//decalre variables to hold the viewport size
+var max_crates = 10;
 
-//enable the matter engine
-engine = Matter.Engine.create();
-world = engine.world;
-body = Matter.Body;
+//declare global variables to hold the framework objects
+var viewport, world, engine, body, elastic_constraint;
 
-// instantiate classes
-var ground = new c_ground(vp_width/2, vp_height-10, vp_width, 20);
-var crate = new c_crate(get_random(500, 650), 400, 120, 120);
-var fuzzball = new c_fuzzball (400, 200, 60);
+var crate = []; //create an empty array that will be used to hold all the crate instances
+var ground;
+var leftwall;
+var rightwall;
+
+var fuzzball;
+var launcher;
+
+// // instantiate classes
+// var ground = new c_ground(vp_width/2, vp_height-10, vp_width, 20);
+// var crate = new c_crate(get_random(500, 650), 400, 120, 120);
+// var fuzzball = new c_fuzzball (400, 200, 60);
 
 function apply_velocity() {
-	Matter.Body.setVelocity(fuzzball.body, {x:10, y:-5});
+	Matter.Body.setVelocity( fuzzball.body, {x: get_random(0, 20), y: get_random(0, 20)*-1});
 };
 
 function apply_angularvelocity() {
-	Matter.Body.setAngularVelocity(crate.body, Math.PI/6);
+	for (let i = 0; i < crate.length; i++ ){
+		Matter.Body.setAngularVelocity( crate[i].body, Math.PI/get_random(3, 20));
+	}	
 };
 
 function apply_force() {
-	Matter.Body.applyForce(crate.body, {
-		x: crate.body.position.x,
-		y: crate.body.position.y
-	}, {
-		x: 0.05,
-		y: -500.05
-	});
+	for (let i = 0; i < crate.length; i++) {
+		Matter.Body.applyForce( crate[i].body, {
+			x: crate[i].body.position.x,
+			y: crate[i].body.position.y
+		}, {
+			x: 0.05,
+			y: get_random(50, 200)*-1
+		});
+	}	
 };
-
 
 function get_random(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min) + min) // the max is exclusive and the min is 
+	return Math.floor(Math.random() * (max - min) + min) // the max is exclusive and the min is inclusive
 }
 
 function preload() {
@@ -45,8 +54,40 @@ function preload() {
 
 function setup() {
 	//this p5 defined function runs automatically once the preload function is done
-	var viewport = createCanvas(vp_width, vp_height); // set the viewport (canvas) size
+	viewport = createCanvas(vp_width, vp_height); // set the viewport (canvas) size
 	viewport.parent("viewport_container"); // move the canvas so it's inside the target div
+	
+	//enable the matter engine
+	engine = Matter.Engine.create();
+	world = engine.world;
+	body = Matter.Body;
+
+	// enable the 'matter' mouse controller and attach it to the viewport object using p5s elt property
+	let vp_mouse = Matter.Mouse.create(viewport.elt);
+	vp_mouse.pixelRatio = pixelDensity(); // update the pixel ratio with the p5 density value this supports
+	//retina screens, etc
+	let options = {
+		mouse: vp_mouse
+	}
+
+	// see docs on https://brm.io/matter-js/docs/classes/Constraint.html#properties
+	elastic_constraint = Matter.MouseConstraint.create(engine, options);
+	Matter.World.add(world, elastic_constraint); // add the elastic constraint object to the world
+
+	ground = new c_ground(vp_width/2, vp_height-10, vp_width, 20); // create a ground object
+	leftwall = new c_ground(0, vp_height/2, 20, vp_height);
+	rightwall = new c_ground(vp_width, vp_height/2, 20, vp_height);
+
+	fuzzball = new c_fuzzball(400, 200, 60); // create a fuzzball object
+
+	//loop through each of the crate indexes
+	for(let i = 0; i < max_crates; i++) { //loop for each instance of a crate
+		crate[i] = new c_crate(700, (150*i)-200, 120, 120);		
+	} 
+
+	//create a launcher object using the fuzzball body
+	launcher = new c_launcher(400, 200, fuzzball.body);
+
 	frameRate(60);
 }
 
@@ -54,23 +95,57 @@ function setup() {
 function paint_background() {
 	// access the game object for the world, use this as a background image for the game
 	background('#4c738b');
+
+	ground.show(); // execute the show function for the boundary objects
+	leftwall.show();
+	rightwall.show();
 }
 
 
 function paint_assets() {
-	ground.show();
-	crate.show();
-	fuzzball.show();
+	for (let i = 0; i < crate.lenght; i++){ // Loop through the crate array and show each
+		crate[i].show()
+	}
+	fuzzball.show(); //show the fuzzball
+	launcher.show(); //show the launcher indicator
 }
 
 
 function draw() {
 	//this p5 defined function runs every refresh cycle
-	paint_background();
-	Matter.Engine.update(engine);
-	paint_assets();
+	paint_background(); // paint the default background 
+	
+	Matter.Engine.update(engine); // run the matter engine update
+	paint_assets(); // paint the assets
+
+	if(elastic_constraint.body !== null) {
+		let pos = elastic_constraint.body.position; // create a shortcut alias
+		fill("#ff0000"); // set a fill colour
+		ellipse(po.x, pos.y, 20, 20); // indicate the body that has been selected 
+
+		let mouse = elastic_constraint.mouse.position;
+		stroke("#00ff00");
+		line(pos.x, pos.y, mouse.x, mouse.y);
+	}
 }
 
 
+function keyPressed() {
+	if (keyCode === ENTER) {
+		console.log("enter key press");
+		fuzzball = new c_fuzzball(400, 200, 60);
+	}
+
+	if (keyCode === 32) {
+		console.log("space key press");
+		launcher.release(); // execute the release method
+	}
+}
+
+function mouseReleased() {
+	setTimeout(() => {
+		launcher.release();
+	}, 100);
+}
 
 
