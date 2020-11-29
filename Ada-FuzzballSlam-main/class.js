6d655caff9b24b3e57d0dbfbf7d5b4d5e1597595
Anna Canby Monk <annacanbymonk@Annas-MBP.config>
2020-11-29 17:10:51 +0000

    Temp commit 2 29/11 17:00

--- Ada-FuzzballSlam-main/class.js
+++ Ada-FuzzballSlam-main/class.js
@@ -176,36 +176,3 @@ class c_fuzzball {
         pop();
     }
 }
-
-// class c_ground {
-//     constructor(x, y, width, height) {
-//         let options = {
-//             isStatic: true,
-//             restitution: 0.99,
-//             friction: 0.20,
-//             density: 1, 
-//             collisionFilter: {
-//                 category: defaultCategory
-//             }          
-//         }
-//         //create the body 
-//         this.body = Matter.Bodies.rectangle(x, y, width, height, options);
-//         Matter.World.add(world, this.body); // add to the matter world
-
-//         this.x = x;
-//         this.y = y;
-//         this.width = width;
-//         this.height = height;        
-//     }
-
-//     body() {
-//         return this.body; // return the created body
-//     }
-
-//     show() {
-//         let pos = this.body.position; // create a shortcut alias
-//         rectMode(CENTER); // switch centre to be centre rather than left, top
-//         //fill("#ffffff"); // set the fill colour
-//         //rect(pos.x, pos.y, this.width, this.height) // draw the rectangle 
-//     }
-// }
